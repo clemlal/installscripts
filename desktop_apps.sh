eab7ff46b9a71d76c40a080f0adb7bd6dfb4ff69
clemlal <clement.lalanne@ens.fr>
2020-05-02 16:20:59 +0200

    better compart

--- desktop_apps.sh
+++ desktop_apps.sh
@@ -1,1 +1,56 @@
 #!/bin/bash
+
+#Adding deb packages
+echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
+sudo apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
+
+#Update
+sudo apt update
+
+#Install my favorite programs
+
+sudo apt install audacity
+
+sudo apt install firefox-esr
+
+sudo apt install thunderbird
+
+sudo apt install timeshift
+
+sudo apt install vlc
+
+sudo apt install uget
+
+sudo apt install transmission
+
+sudo apt install gedit
+
+sudo apt install virt-manager
+
+sudo apt install gnome-disk-utility
+
+sudo apt install opam 
+
+sudo apt install julia
+
+sudo apt install texlive
+
+sudo apt install balena-etcher-electron
+
+sudo apt install gimp
+
+sudo apt install libreoffice
+
+sudo apt install obs-studio
+
+sudo apt install evince
+
+sudo apt install eog
+
+sudo apt install baobab
+
+sudo apt install kdenlive kdenlive-data kde-runtime kde-style-breeze kde-style-breeze-qt4
+
+sudo apt install ardour ardour-data ardour-video-timeline
+
+sudo apt install calf-plugins
--- gnome.sh
+++ gnome.sh
@@ -8,94 +8,29 @@
 #Install gnome desktop environment
 #tasksel install desktop gnome-desktop
 
-apt update
-apt upgrade
+sudo apt update
+sudo apt upgrade
 
-apt install xserver-xorg xserver-xorg-core xfonts-base xinit --no-install-recommends
+sudo apt install xserver-xorg xserver-xorg-core xfonts-base xinit --no-install-recommends
 
-apt install libgl1-mesa-dri x11-xserver-utils gnome-session gnome-shell gnome-terminal gnome-control-center nautilus gnome-icon-theme --no-install-recommends
+sudo apt install libgl1-mesa-dri x11-xserver-utils gnome-session gnome-shell gnome-terminal gnome-control-center nautilus gnome-icon-theme --no-install-recommends
 
-apt install gdm3 --no-install-recommends
+sudo apt install gdm3 --no-install-recommends
 
 #apt install network-manager-openvpn-gnome
 
 # Install the build essentials
-tasksel install laptop
-apt install curl build-essential git
-apt install sudo
+sudo apt install curl build-essential git
 
 #Set graphical at start 
 #systemctl set-default graphical.targetapt
 
 #Install the theme
-apt install gnome-tweaks
-apt install gnome-shell-extensions
+sudo apt install gnome-tweaks
+sudo apt install gnome-shell-extensions
 
-apt install breeze-cursor-theme arc-theme materia-gtk-theme papirus-icon-theme
+sudo apt install breeze-cursor-theme arc-theme materia-gtk-theme papirus-icon-theme
 
-git clone https://github.com/clemlal/backgrounds.git /root/etc/backgrounds 
-mkdir /usr/share/backgrounds/custom
-mv /root/etc/backgrounds/*.jpg /usr/share/backgrounds/custom
+git clone https://github.com/clemlal/backgrounds.git 
 
-#Adding deb packages
-echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
-apt-key adv --keyserver hkps://keyserver.ubuntu.com:443 --recv-keys 379CE192D401AB61
 
-#Update
-apt update
-
-#Install my favorite programs
-
-apt install audacity
-
-apt install firefox-esr
-
-apt install thunderbird
-
-apt install timeshift
-
-apt install vlc
-
-apt install uget
-
-apt install transmission
-
-apt install gedit
-
-apt install virt-manager
-
-apt install gnome-disk-utility
-
-apt install opam 
-
-apt install julia
-
-apt install texlive
-
-apt install balena-etcher-electron
-
-apt install gimp
-
-apt install libreoffice
-
-apt install obs-studio
-
-apt install evince
-
-apt install eog
-
-apt install baobab
-
-apt install kdenlive kdenlive-data kde-runtime
-
-apt install ardour ardour-data ardour-video-timeline
-
-apt install calf-plugins
-
-#Import Gnome Preferences
-
-#Import dotfiles
-
-#ST
-
-#VIM
